53269bc06e52e08634877f8ca108630a8b9377bf
Gustavo Carletto <gucarletto@gmail.com>
2021-09-06 16:50:12 -0300

    Initial commit

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => res.send('Server running!'));
+
+export default routes;
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import app from './app';
+
+app.listen(process.env.PORT, () => console.log(`App running on port ${process.env.PORT}`));
